c8c01c6c593a630e0570844a0713648fc2ad9f4b
Jandy Tete Ruiz <jandyteteruiz@gmail.com>
2025-08-10 21:10:42 -0500

    Añadir funcionalidad para eliminar tareas y actualizar README

--- task_model.py
+++ task_model.py
@@ -2,6 +2,14 @@
 class TaskModel:
     def __init__(self, task_name):
         self.task_name = task_name
+        self.is_completed = False
 
     def get_task_name(self):
         return self.task_name
+
+    def delete_task(self):
+        self.task_name = None
+        self.is_completed = False
+
+    def is_completed(self):
+        return self.is_completed
